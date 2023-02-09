b0ad1ae9afd422bccd329753f297c99962d8d085
Lily <lily.majani@ynov.com>
2023-02-09 14:28:26 +0100

    Init spring boot project

---
+++ src/main/java/tp/popotecar/PopoteCarApplication.java
@@ -0,0 +1,13 @@
+package tp.popotecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopoteCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopoteCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tp/popotecar/PopoteCarApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.popotecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopoteCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
